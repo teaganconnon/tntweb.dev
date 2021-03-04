8b07e7062c785e2ca08d63b930b449bfcc0eb42d
tybens <tb19@princeton.edu>
2021-03-03 21:42:36 -0800

    add basic mobile layout configs

--- src/RouterScreen/Header/styles.js
+++ src/RouterScreen/Header/styles.js
@@ -3,30 +3,42 @@ import { makeStyles } from "@material-ui/core/styles";
 export default makeStyles((theme) => ({
 
     routingLink: {
-        margin: "0 50px",
-        fontSize: "17px"
+        fontSize: "18px"
     },
 
     routingDiv: {
         display: 'flex',
-        justifyContent: 'center'
+        justifyContent: 'space-between',
+        width: "50%",
+        margin: "0 20px",
+        [theme.breakpoints.down('xs')]: {
+            width: "90%",
+
+        }
+        
     },
 
     tntLogo: {
         display: 'flex',
         justifyContent: 'center',
-        width: '100px',
         fontSize: '35px',
-        marginLeft: "35px"
+        marginLeft: "35px",
+        [theme.breakpoints.down('xs')]: {
+            marginLeft: "0",
+            fontSize: "55px",
+        }
     },
     
     headerDiv: {
         padding: "20px 10px",
-        height: "50px",
         display: "flex",
         alignItems: "center",
         justifyContent: "space-between",
-        backgroundColor: "#222222"
+        backgroundColor: "#222222",
+        [theme.breakpoints.down('xs')]: {
+            flexDirection: "column",
+            height: "95px"
+        }
     },
 
     link: {

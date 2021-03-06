b6164c23797ebc76b33f1a70c7c597df6ce88690
teaganconnon <teagan@ucsb.edu>
2021-03-05 21:16:17 -0800

    very rudimentary contact form

---
+++ src/Pages/Contact/ContactForm/index.js
@@ -0,0 +1,50 @@
+import React from 'react'
+class ContactForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: '',
+            email: '',
+            message: ''
+        }
+    }
+    render() {
+        return(
+          <div className="ContactForm">
+            <form id="contact-form" onSubmit={this.handleSubmit.bind(this)} method="POST">
+              <div className="form-group">
+                <label htmlFor="name">Name</label>
+                <input type="text" className="form-control" value={this.state.name} onChange={this.onNameChange.bind(this)} />
+              </div>
+              <div className="form-group">
+                <label htmlFor="exampleInputEmail1">Email address</label>
+                <input type="email" className="form-control" aria-describedby="emailHelp" value={this.state.email} onChange={this.onEmailChange.bind(this)} />
+              </div>
+              <div className="form-group">
+                <label htmlFor="message">Message</label>
+                <textarea className="form-control" rows="5" value={this.state.message} onChange={this.onMessageChange.bind(this)} />
+              </div>
+              <button type="submit" className="btn btn-primary">Submit</button>
+            </form>
+          </div>
+        );
+    }
+    onNameChange(event) {
+        this.setState({name: event.target.value})
+    }
+    
+    onEmailChange(event) {
+        this.setState({email: event.target.value})
+    }
+    
+    onMessageChange(event) {
+        this.setState({message: event.target.value})
+    }
+    
+    handleSubmit(event) {
+        event.preventDefault();
+        console.log(this.state);
+    }
+}
+
+export default ContactForm;
--- src/Pages/Contact/index.js
+++ src/Pages/Contact/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import ContactForm from '../Contact/ContactForm'
 const Contact = () => {
     return (
         <>
             <div style={{ padding: "50px" }}>
                 <h1>Contact</h1>
-                <p>Donec sit amet augue at enim sollicitudin porta. Praesent finibus ex velit, quis faucibus libero congue et. Quisque convallis eu nisl et congue. Vivamus eget augue quis ante malesuada ullamcorper. Sed orci nulla, eleifend eget dui faucibus, facilisis aliquet ante. Suspendisse sollicitudin nibh lacus, ut bibendum risus elementum a.</p>            </div>
+                <ContactForm></ContactForm>            
+            </div>
         </>
     )
 }
